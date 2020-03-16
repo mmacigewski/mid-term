2c367cb3c31d7ad150124f170ae610006379bfbc
mmacigewski <max.macigewski@g.austincc.edu>
2020-03-15 19:02:22 -0500

    made a quick lil change.

--- src/01_expressions/fun_clock.h
+++ src/01_expressions/fun_clock.h
@@ -14,5 +14,4 @@ int get_minutes(int seconds_since_1970);
 Write a function get_seconds that returns an int and accepts an int seconds_since_1970 parameter
 */
 
-
 int get_seconds(int seconds_since_1970);
